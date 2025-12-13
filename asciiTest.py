37c9f20983e16ec67e82ab333c44041d0fd30736
RK-ux12 <raghavbalasankar12@gmail.com>
2025-12-13 13:30:51 +0530

    Fixed Ascii Pixel Ratio Slightly and added support for all PIL versions

--- asciiTest.py
+++ asciiTest.py
@@ -1,58 +1,37 @@
 from PIL import Image, ImageFont, ImageDraw
-import os
 
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
 def image_to_ascii(img, new_width=150):
     width, height = img.size
-    aspect_ratio = height / width
-    new_height = int(new_width * aspect_ratio * 0.45)   # GOOD results
-
-    img = img.resize((new_width, new_height)).convert("L")
-    pixels = img.getdata()
-
-    ascii_str = "".join(ASCII_CHARS[p // 25] for p in pixels)
-    ascii_lines = [ascii_str[i:i+new_width] for i in range(0, len(ascii_str), new_width)]
-
-    return "\n".join(ascii_lines)
-
-
-def save_ascii_as_image(ascii_text, output_path):
     try:
         font = ImageFont.truetype("cour.ttf", size=12)
     except:
         font = ImageFont.truetype("C:\\Windows\\Fonts\\cour.ttf", size=12)
+    bbox = font.getbbox("A")
+    char_width = bbox[2] - bbox[0]
+    char_height = bbox[3] - bbox[1]
+    char_aspect = char_width / char_height
+    new_height = int((height / width) * new_width / char_aspect)
+    img = img.resize((new_width, new_height)).convert("L")
+    pixels = img.getdata()
 
-    lines = ascii_text.split("\n")
+    ascii_str = "".join(
+        ASCII_CHARS[int(p / 255 * (len(ASCII_CHARS) - 1))]
+        for p in pixels
+    )
 
-    char_width, char_height = font.getbbox("A")[2:]
-    
-    img_width = char_width * max(len(line) for line in lines)
-    img_height = char_height * len(lines)
-
-    img = Image.new("RGB", (img_width, img_height), "white")
-    draw = ImageDraw.Draw(img)
-
+    ascii_lines = [
+        ascii_str[i:i + new_width].ljust(new_width, ASCII_CHARS[-1])
+        for i in range(0, len(ascii_str), new_width)
+    ]
+    img_width = char_width * new_width
+    img_height = char_height * new_height
+    ascii_img = Image.new("RGB", (img_width, img_height), "white")
+    draw = ImageDraw.Draw(ascii_img)
     y = 0
-    for line in lines:
+    for line in ascii_lines:
         draw.text((0, y), line, fill="black", font=font)
         y += char_height
 
-    img.save(output_path)
-
-
-def convert_image_to_ascii_image(input_path):
-    img = Image.open(input_path)
-
-    ascii_art = image_to_ascii(img)
-
-    base, ext = os.path.splitext(input_path)
-    output_path = base + "_ascii.png"
-
-    save_ascii_as_image(ascii_art, output_path)
-
-    print("Saved:", output_path)
-
-
-# Test:
-convert_image_to_ascii_image("image10.jpg")
+    return ascii_img
